4750119e514c795cc9f44a5a97f97b378bc27bcf
TheRealSesJ <96968837+TheRealSesJ@users.noreply.github.com>
2022-06-21 16:54:58 -0700

    Update InputController.java
    Input Controller uses enemy stats for enemy readout now
    Refactored the input getters to be type specific with a normal one that is used by, for easier extension in the future

--- src/main/java/com/sesj/InputController.java
+++ src/main/java/com/sesj/InputController.java
@@ -26,61 +26,94 @@ public class InputController{
     //game loop
     while(playing){
       
-      //take turn
+//----------------------------take turn
       try{
-        while(!scanInput(SCAN.nextLine(), player)){
+        while(!scanWorldInput(SCAN.nextLine(), player)){
           p.println("please enter an action");
         }
         p.println("\nNext turn:\n");
+
+//----------------------check for combat, if yes convert to *combat turns*
+        while(World.getLocation(player).getEnemy()!=null){
+          p.println("\nAn enemy approaches...\n");
+          p.println(World.getLocation(player).getEnemy().getStats());
+
+          while(!scanCombatInput(SCAN.nextLine(), player)){
+          p.println("please enter an action");
+          }
+  
+//------check for game over condition
+          if(player.getHp()<=0){
+            playing = false;
+            break;
+          }
+          
+          p.println("\nNext turn:\n");
+        }
       }
-//----------------handles game end
+//----------------handles game end with exception from end_game input
       catch(InterruptedIOException e){
         playing = false;
         break;
       }
-      
 
-      //check for combat, if yes convert to *combat turns*
-      while(World.getLocation(player).getEnemy()!=null){
-        p.println("\nan enemy approaches...\n");
-        p.println("enemy is: "+ World.getLocation(player).getEnemy().toString());
-        p.println("Hp: "+World.getLocation(player).getEnemy().getHp());
-        while(!scanCombatInput(SCAN.nextLine(), player)){
-        p.println("please enter an action");
-        }
-
-//------check for game over condition
-        if(player.getHp()<=0){
-          playing = false;
-          break;
-        }
-        p.println("\nNext turn:\n");
-      }
-
-//-----check for game over condition
+//-----check for game over condition again
       if(player.getHp()<=0){
         playing = false;
         break;
       }
 
     }
+
+
+
 //----end of the game----
     p.println("\nGAME OVER\n");
-    try{
-      Thread.sleep(2000);
-    }
-    catch(InterruptedException e){}
+
+    p.println("Press any button to exit:");
+    SCAN.nextLine();
   }
 
-//input mappings for normal inputs, uses up one turn if a correct input is executed
+
+  public static boolean scanWorldInput(String input, Player player) throws InterruptedIOException{
+    String[] inputArr = input.split(" ");
+    //normal inputs
+    if(scanInput(input, player)) return true;
+    //world inputs
+    switch(inputArr[0].toLowerCase()){
+      case "move":
+        return GameController.translate(inputArr, player);
+      case "grab_item":
+        return GameController.grabItem(player);
+      default:
+        return false;
+    }
+  }
+
+//input mappings for combat, works in similar fashion to main commands
+  public static boolean scanCombatInput(String input, Player player) throws InterruptedIOException{
+    String[] inputArr = input.split(" ");
+    //normal inputs
+    if(scanInput(input, player)) return true;
+    //combat inputs
+    switch(inputArr[0].toLowerCase()){
+      case "fight":
+        return GameController.combat(player);
+      case "run":
+        return GameController.run(player);
+      default:
+        return false;
+    }
+  }
+
+
+  //input mappings for normal inputs, uses up one turn if a correct input is executed
   //methods here can return false in order to not advance the player turn, namely the help function
   public static boolean scanInput(String input, Player player) throws InterruptedIOException{
     String[] inputArr = input.split(" ");
     switch(inputArr[0].toLowerCase()){
       case "scan":
         return GameController.scan(inputArr, player);
-      case "move":
-        return GameController.translate(inputArr, player);
       case "interact":
         return GameController.interact(inputArr, player);
       case "minimap": //always false
@@ -89,51 +122,12 @@ public class InputController{
         return GameController.displayStats(player);
       case "help": //always false
         return GameController.getHelp();
-      case "grab_item":
-        return GameController.grabItem(player);
       case "end_game":
         return GameController.endGame();
       default:
         return false;
     }
   }
-
-
-//input mappings for combat, works in similar fashion to main commands
-  public static boolean scanCombatInput(String input, Player player){
-    String[] inputArr = input.split(" ");
-    switch(inputArr[0].toLowerCase()){
-      case "fight":
-        return GameController.combat(player);
-      case "scan": 
-        return GameController.scan(inputArr, player);
-      case "run":
-        return GameController.run(player);
-      case "minimap": //always false
-        return GameController.displayMinimap(player);
-      case "stats": //always false
-        return GameController.displayStats(player);
-      case "interact":
-        return GameController.interact(inputArr, player);
-      case "help": //always false
-        return GameController.getHelp();
-      default:
-        return false;
-    }
-  }
-
-
-//end game
-public static boolean scanContinue(String input, Player player){
-  String[] inputArr = input.split(" ");
-  switch(inputArr[0].toLowerCase()){
-    case "end_game":
-      return true; //does not need to be evaluated
-    default:
-      return false; //game continues
-  }
-}
-  
 
   
 }
